c11ab98f4bd8e8bf5b6fd73ed8a08b5fba74cd51
Ziad Mostafa <ziad.mostafa@apex.ai>
2022-12-19 13:05:17 +0100

    Add protective call to harpoon config

--- lua/user/harpoon.lua
+++ lua/user/harpoon.lua
@@ -1,5 +1,14 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+-- local mark = require("harpoon.mark")
+local mark_status_ok, mark = pcall(require, "harpoon.mark")
+if not mark_status_ok then
+  return
+end
+
+-- local ui = require("harpoon.ui")
+local ui_status_ok, ui = pcall(require, "harpoon.ui")
+if not ui_status_ok then
+  return
+end
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
