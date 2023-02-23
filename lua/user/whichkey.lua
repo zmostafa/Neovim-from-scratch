076d4c3d61d1fd16814c5b0bbab800014001d3eb
Ziad Mostafa <ziad.mostafa@apex.ai>
2023-02-23 11:54:07 +0100

    Update key name

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -95,7 +95,7 @@ local mappings = {
   },
   ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
   ["P"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
-  ["n"] = { "<cmd>lua require('neogen').generate()<CR>", "annotaion" },
+  ["n"] = { "<cmd>lua require('neogen').generate()<CR>", "Doxygen Annotaion" },
 
   p = {
     name = "Packer",
