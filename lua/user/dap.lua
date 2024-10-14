d14a413bfcb475110973416878f0d883f9fa9016
Ziad Mostafa <zmostafa@nvidia.com>
2024-10-14 16:46:06 +0200

    enbale debugging for C/C++ and rust
    Signed-off-by: Ziad Mostafa <zmostafa@nvidia.com>

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -8,15 +8,77 @@ if not status_dapui_ok then
   return
 end
 
-local status_dapvtext_ok, dapvtext = pcall(require, 'nvim-dap-virtual-text')
-if not status_dapvtext_ok then
-  return
-end
+-- local status_dapvtext_ok, dapvtext = pcall(require, 'nvim-dap-virtual-text')
+-- if not status_dapvtext_ok then
+--   return
+-- end
+--
+-- dapvtext.setup()
+--
+vim.fn.sign_define('DapBreakpoint', { text='🐞', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
 
-dapvtext.setup()
-
-vim.fn.sign_define('DapBreakpoint', { text='🔴', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
 -- dap UI config
+dapui.setup({
+  controls = {
+    element = "repl",
+    enabled = true,
+    icons = {
+      disconnect = "",
+      pause = "",
+      play = "",
+      run_last = "",
+      step_back = "",
+      step_into = "",
+      step_out = "",
+      step_over = "",
+      terminate = ""
+    }
+  },
+  element_mappings = {},
+  expand_lines = true,
+  floating = {
+    border = "single",
+    mappings = {
+      close = { "q", "<Esc>" }
+    }
+  },
+  force_buffers = true,
+  icons = {
+    collapsed = "",
+    current_frame = "",
+    expanded = ""
+  },
+  layouts = {
+    {
+      elements = {
+        "scopes"
+      },
+      size = 0.3,
+      position = "bottom",
+    },
+    {
+      elements = {
+        "repl",
+        "breakpoints"
+      },
+      size = 0.3,
+      position = "right"
+    },
+  },
+  mappings = {
+    edit = "e",
+    expand = { "t", "<2-LeftMouse>" },
+    remove = "d",
+    repl = {},
+    open = {},
+    toggle = {},
+  },
+  render = {
+    indent = 1,
+    max_value_lines = 100
+  }
+})
+
 dap.listeners.before.attach.dapui_config = function()
   dapui.open()
 end
@@ -35,8 +97,8 @@ dap.adapters.codelldb = {
   port = "${port}",
   executable = {
     -- CHANGE THIS to your path!
-    command = '/home/zmostafa/Downloads/codelldb-darwin-x64/extension/adapter/codelldb',
-    args = {"--port", "${port}"},
+    command = '/home/zmostafa/Downloads/codelldb-linux-x64/extension/adapter/codelldb',
+    args = { "--port", "${port}" },
 
     -- On windows you may have to uncomment this:
     -- detached = false,
