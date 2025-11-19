ea335b40f6aa0ab37a20541f4194888800e33c3e
Ziad Mostafa <zmostafa@nvidia.com>
2025-11-19 21:31:50 +0100

    change transparency

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,8 +1,17 @@
--- local colorscheme = "tokyonight"
+local colorscheme = "tokyonight"
 -- local colorscheme = "darkplus"
-local colorscheme = "solarized-osaka"
+-- local colorscheme = "solarized-osaka"
+
+require("tokyonight").setup {
+    transparent = true,
+    styles = {
+       sidebars = "transparent",
+       floats = "transparent",
+    }
+}
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
+
